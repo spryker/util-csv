6022794f00c7d0ef24306254c204ef8bfd11c876
Roman S <shopin.roman@gmail.com>
2018-07-04 16:39:48 +0300

    [WIP] PS-1161: Bugfix (#3404)

--- src/Spryker/Service/UtilCsv/Exporter/FileExporterInterface.php
+++ src/Spryker/Service/UtilCsv/Exporter/FileExporterInterface.php
@@ -5,32 +5,14 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Service\UtilCsv;
+namespace Spryker\Service\UtilCsv\Exporter;
 
 use Generated\Shared\Transfer\CsvFileTransfer;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
-interface UtilCsvServiceInterface
+interface FileExporterInterface
 {
     /**
-     * Specification:
-     * - Read data from uploaded csv file and returns content
-     *
-     * @api
-     *
-     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     *
-     * @return array
-     */
-    public function readFile(UploadedFile $file): array;
-
-    /**
-     * Specification:
-     * - Generates csv file and returns response with file for export
-     *
-     * @api
-     *
      * @param \Generated\Shared\Transfer\CsvFileTransfer $csvFileTransfer
      *
      * @return \Symfony\Component\HttpFoundation\StreamedResponse
